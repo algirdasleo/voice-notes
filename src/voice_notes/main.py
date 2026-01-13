b982dc74033123564b29169bbce1bb72bc7acd14
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2026-01-13 18:41:43 +0200

    add initialization of repositories and addition to session state

--- src/voice_notes/main.py
+++ src/voice_notes/main.py
@@ -4,7 +4,17 @@ from pathlib import Path
 
 import streamlit as st
 
+from voice_notes.repositories.content import ContentRepository
+from voice_notes.repositories.notes import NotesRepository
+from voice_notes.services.database import create_tables
+
 st.set_page_config(layout="wide")
+
+create_tables()
+
+if "notes_repo" not in st.session_state:
+    st.session_state.notes_repo = NotesRepository()
+    st.session_state.content_repo = ContentRepository()
 
 pages = [
     st.Page(
@@ -13,7 +23,9 @@ pages = [
     st.Page(
         Path("pages/content.py"), title="Content"
     ),  # Select Notes and create: meeting report, to-do list, translate, blog post, email, summary, custom prompt
-    st.Page(Path("pages/ai.py"), title="AI"),  # Ask AI questions based on all notes
+    st.Page(
+        Path("pages/chat.py"), title="Notes AI"
+    ),  # Ask AI questions based on all notes
 ]
 
 nav = st.navigation(pages, position="top")
