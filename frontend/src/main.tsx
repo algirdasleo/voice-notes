f9d6214a9a8e429926bae243ea8c1de5e8dbaa8a
Algirdas Poskus <AlgirdasLeonardas@gmail.com>
2026-01-28 21:07:45 +0200

    remove theme provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,15 +4,12 @@ import "@radix-ui/themes/styles.css";
 import "./index.css";
 
 import { Theme } from "@radix-ui/themes";
-import { ThemeProvider } from "./context/ThemeContext";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <Theme appearance="inherit">
-        <App />
-      </Theme>
-    </ThemeProvider>
+    <Theme appearance="inherit">
+      <App />
+    </Theme>
   </StrictMode>
 );
